64014431272de06ae46a29c01b6990740ed923d6
Tatiana Kholod <tkholod@yandex.ru>
2023-07-10 15:09:16 +0300

    fix(пр15): исправления lintera

--- frontend/src/utils/Auth.js
+++ frontend/src/utils/Auth.js
@@ -1,10 +1,10 @@
 export const BASE_URL = process.env.REACT_APP_BASE_URL
-  ? document.location.protocol + '//' + process.env.REACT_APP_BASE_URL
-  : 'http://localhost:3000';
+  ? document.location.protocol + "//" + process.env.REACT_APP_BASE_URL
+  : "http://localhost:3000";
 
 const headerJson = {
-  Accept: 'application/json',
-  'Content-Type': 'application/json',
+  Accept: "application/json",
+  "Content-Type": "application/json",
 };
 
 const checkResponse = (res) => {
@@ -19,38 +19,38 @@ const request = (urlEndPoint, options) => {
 };
 
 export const register = (email, password) => {
-  return request('/signup', {
-    method: 'POST',
-    credentials: 'include',
+  return request("/signup", {
+    method: "POST",
+    credentials: "include",
     headers: headerJson,
     body: JSON.stringify({ email, password }),
   });
 };
 
 export const autorize = (email, password) => {
-  return request('/signin', {
-    method: 'POST',
-    credentials: 'include',
+  return request("/signin", {
+    method: "POST",
+    credentials: "include",
     headers: headerJson,
     body: JSON.stringify({ email, password }),
   });
 };
 
 export const unAutorize = () => {
-  return request('/signin', {
-    method: 'DELETE',
-    credentials: 'include',
+  return request("/signin", {
+    method: "DELETE",
+    credentials: "include",
     headers: headerJson,
   });
 };
 
 export const getToken = () => {
-  return request('/users/me', {
-    method: 'GET',
-    credentials: 'include',
+  return request("/users/me", {
+    method: "GET",
+    credentials: "include",
     headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
+      Accept: "application/json",
+      "Content-Type": "application/json",
     },
   });
 };
